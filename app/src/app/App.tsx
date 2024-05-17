f00efe4468c2a7ed126b9f6a307f6c4cdf793035
CrazyHuman00 <asakurashi00@icloud.com>
2024-05-18 02:53:07 +0900

    ボタンお配置

---
+++ app/src/app/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+          <a className="btn-emergency">
+              <span className="btn-emergency-bottom"></span>
+              <span className="btn-emergency-top"><span>ガチャ</span></span>
+          </a>
+      </div>
+  );
+}
+
+export default App;
